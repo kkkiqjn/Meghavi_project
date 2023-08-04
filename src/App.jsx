f8ddb0f68afee65c04cd802d55564416f892170e
Karkala Vikas reddy <71484696+kkkiqjn@users.noreply.github.com>
2023-08-04 17:35:48 +0530

    initial comiit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Headline from './components/headline'
+import Formm from './components/formcomp'
+import './App.css'
+import "bootstrap/dist/css/bootstrap.css"
+function App() {
+
+
+  return (
+    <>
+       <Headline />
+   <Formm />
+    </>
+
+  )
+}
+
+export default App
---
+++ src/components/headline.jsx
@@ -0,0 +1,10 @@
+import "C:\\Users\\saket\\project_meghavi\\src\\headline.css"
+function Headline(){
+    return (
+<div className="Headdiv  ">
+    <h1 className="heading ">Book Appointment</h1>
+</div>
+
+    )
+}
+export default Headline;
